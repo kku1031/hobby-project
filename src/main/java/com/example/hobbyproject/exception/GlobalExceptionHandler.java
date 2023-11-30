4da0715714304ff21e96084035adcfb29e09a820
Kanggyeonggu <kku1031@naver.com>
2023-11-30 18:08:32 +0900

    exception 처리 구조 변경

--- src/main/java/com/example/hobbyproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/hobbyproject/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.hobbyproject.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,18 +7,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(AlreadyDeletedException.class)
-    public ResponseEntity<String> handleAlreadyDeletedException(AlreadyDeletedException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.OK);
+    @ExceptionHandler(PostException.class)
+    public ResponseEntity<String> handlePostException(PostException exception) {
+        return new ResponseEntity<>(exception.getMessage(), exception.getExceptionType().getHttpStatus());
     }
 
-    @ExceptionHandler(DuplicatePostException.class)
-    public ResponseEntity<String> handleDuplicatePostException(DuplicatePostException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(PostNotFoundException.class)
-    public ResponseEntity<String> handlePostNotFoundException(PostNotFoundException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    }
 }
